f3a2b613d9d72b5f0d4365f867cdce84923dff3e
Sanya <160408798+LovkiiSanya@users.noreply.github.com>
2024-05-23 19:09:02 +0300

    main.py

--- Flask project/main.py
+++ Flask project/main.py
@@ -6,7 +6,6 @@ from courses import *
 app = Flask(__name__)
 api = Api(app)
 
-# Регистрация Blueprint'ов
 app.register_blueprint(courses_bp)
 app.register_blueprint(users_bp)
 app.register_blueprint(user_course_bp)
